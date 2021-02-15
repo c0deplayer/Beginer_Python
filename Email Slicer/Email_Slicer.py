003c002e858d27dc4d8a1d436193c1579fd16490
JakubKujawa <Kujawa_Jakub@yahoo.com>
2021-02-15 17:43:14 +0100

    Small changes

--- Email Slicer/Email_Slicer.py
+++ Email Slicer/Email_Slicer.py
@@ -1,14 +1,14 @@
 # Author: CodePlayer
 # Date: 15.02.2021
 
-def main():
+def main():   # Nothing interesting to see here
     email = input("Enter an email address: ").strip()
     username = email[:email.index('@')]
     domain = email[email.index('@') + 1:]
     print(f'''
     The program successfully get the username and domain name from an email address
-        Username: {username}
-        Domain: {domain}
+      > Username: {username}
+      > Domain: {domain}
     ''')
 
 
