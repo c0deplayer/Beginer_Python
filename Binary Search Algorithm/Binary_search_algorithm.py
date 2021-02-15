07f138bc445f3ffa4d5d8c078e0ddea005611efb
JakubKujawa <Kujawa_Jakub@yahoo.com>
2021-02-15 14:48:06 +0100

    Add protection against out-of-range numbers and new way to exit

--- Binary Search Algorithm/Binary_search_algorithm.py
+++ Binary Search Algorithm/Binary_search_algorithm.py
@@ -4,7 +4,7 @@ import random
 
 
 def random_list():
-    return random.sample(range(0, 101, 2), 40)
+    return random.sample(range(0, 101, 2), 40)   # Returns a list of 40 randoms numbers
 
 
 def searching(number, array):
@@ -21,18 +21,27 @@ def searching(number, array):
             end = mid - 1
         else:
             start = mid + 1
-    return -1
+    return "Number not found"
 
 
 def main():
+    print('''
+    =================================
+         Binary Search Algorithm
+    =================================
+     ''')
     while True:
         array = random_list()
-        number = int(input("Enter a number between <0, 100>\n"))
-        print(f"Index of {number}: {searching(number, sorted(array))}")
-        again = input("Do you want to continue? [y/n] ")
-        if again not in ['y', 'yes', 'ok']:
+        number = input("Enter a number between <0, 100> ('q' to leave the program)\n")
+
+        if number.lower() in ['q', 'quit', 'exit']:
             print("Goodbye!")
             break
+
+        if int(number) not in range(0, 101):
+            print("You entered a number outside the range!")
+        else:
+            print(f"Index of {number}: {searching(int(number), sorted(array))}")
 
 
 if __name__ == '__main__':
