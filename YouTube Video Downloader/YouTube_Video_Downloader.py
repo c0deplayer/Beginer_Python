51cbd18a6b9ecfd5f54b1847e3eeab47a99d9e6a
JakubKujawa <Kujawa_Jakub@yahoo.com>
2021-02-19 10:21:05 +0100

    Remove downloading video w/o audio

--- YouTube Video Downloader/YouTube_Video_Downloader.py
+++ YouTube Video Downloader/YouTube_Video_Downloader.py
@@ -3,43 +3,43 @@
 # IMPORTANT: You need to install ffmpeg for the audio to video merging to take place!
 import ffmpeg
 import os
-import time
 from pytube import YouTube
 
 
-def download_video(choice, yt, resolution):
-    file_name = input("What should the downloaded file be called? (Can't contain spaces!)?\n")
+def download_video(yt, resolution):
+    print('''
+                              +==============+
+                                IMPORTANT!!!
+                              +==============+
+    The script will download the video and audio to merge them together. 
+          Otherwise the video would be without sound (limitations)
+    ''')
     print("Downloading video...")
-    yt.streams.filter(res=resolution, mime_type="video/mp4").first().download(filename=file_name)
+    try:
+        yt.streams.filter(res=resolution, mime_type="video/mp4").first().download(filename='temp_v')
+    except AttributeError:
+        print("The selected resolution is not available\nDownloading the highest resolution...")
+        yt.streams.filter(adaptive=True, mime_type="video/mp4").first().download(filename='temp_v')
     print("Done!")
-
-    if resolution not in ['240p', '360p'] and choice == '1':
-        print('''
-                                      +==============+
-                                        IMPORTANT!!!
-                                      +==============+
-        Because you have selected a video with audio at a resolution higher than 360p 
-        we also need to download the audio to merge it with the video (limitations)
-        ''')
-        file_video = ffmpeg.input(file_name + ".mp4")
-        time.sleep(7)
-        print("Downloading audio...")
-        yt.streams.filter(mime_type="audio/mp4").first().download(filename="temp_a")
-        file_audio = ffmpeg.input('temp_a.mp4')
-        print("Done!!")
-        merging(file_video, file_audio, file_name + "(M).mp4")
-        clearing(file_name + ".mp4")
+    file_video = ffmpeg.input('temp_v.mp4')
+    print("Downloading audio...")
+    yt.streams.filter(mime_type="audio/mp4").first().download(filename='temp_a')
+    file_audio = ffmpeg.input('temp_a.mp4')
+    print("Done!!")
+    file_name = input("\nWhat should the downloaded file be called?\n") + '.mp4'
+    merging(file_video, file_audio, file_name)
+    clearing()
 
 
-def merging(video, audio, file_name):
+def merging(video, audio, name):
     print("Merging...")
-    ffmpeg.concat(video, audio, v=1, a=1).output(file_name).run()
+    ffmpeg.concat(video, audio, v=1, a=1).output(name).run()
     print("Done!!")
 
 
-def clearing(file_name):
+def clearing():
     print("Removal of residual...")
-    os.remove(file_name)
+    os.remove('temp_v.mp4')
     os.remove('temp_a.mp4')
     print("Done!!!")
 
@@ -51,23 +51,22 @@ def main():
              Welcome to Youtube Video Downloader (Alpha!!!)
         ========================================================
         
-            1. Download video w/ audio
-            2. Download video w/o audio
-            3. Download only audio
-            4. Exit
+            1. Download video
+            2. Download audio
+            3. Exit
         ''')
         choice = input("Enter one of the available option: ")
-        if choice == '4':
+        if choice == '3':
             print("Goodbye!")
             break
 
-        link = input("Please paste (or enter) the link from where you want to download the video\n")
+        link = input("Paste (or enter) the link to the YouTube video\n")
         yt = YouTube(link)
         print(f"Video title: {yt.title}")
-        if choice in ['1', '2']:
+        if choice == '1':
             resolution = input("Specify the resolution you want to download (max for now. 1080p): ")
-            download_video(choice, yt, resolution)
-        elif choice == '3':
+            download_video(yt, resolution)
+        elif choice == '2':
             print("WIP")
         else:
             print("Invalid options!\nPlease, try again")
