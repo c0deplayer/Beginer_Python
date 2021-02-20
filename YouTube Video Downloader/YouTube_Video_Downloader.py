1a99ba7281414db91c7d46c1131f1c41107f327f
JakubKujawa <Kujawa_Jakub@yahoo.com>
2021-02-20 12:24:26 +0100

    Use pathlib to delete the files

--- YouTube Video Downloader/YouTube_Video_Downloader.py
+++ YouTube Video Downloader/YouTube_Video_Downloader.py
@@ -2,7 +2,7 @@
 # Date: 17.02.2021
 # IMPORTANT: You need to install ffmpeg for the audio to video merging to take place!
 import ffmpeg
-import os
+import pathlib
 from pytube import YouTube
 
 
@@ -10,7 +10,7 @@ def download_audio(yt):
     file_name = input("\nWhat should the downloaded audio file be called?\n")
     print("Downloading audio...")
     yt.streams.filter(mime_type="audio/mp4").first().download(filename=file_name)
-    print("Done!!")
+    print("Done!!!")
 
 
 def download_video(yt, resolution):
@@ -27,12 +27,10 @@ def download_video(yt, resolution):
     except AttributeError:
         print("The selected resolution is not available\nDownloading the highest resolution...")
         yt.streams.filter(adaptive=True, mime_type="video/mp4").first().download(filename='temp_v')
-    print("Done!")
     file_video = ffmpeg.input('temp_v.mp4')
     print("Downloading audio...")
     yt.streams.filter(mime_type="audio/mp4").first().download(filename='temp_a')
     file_audio = ffmpeg.input('temp_a.mp4')
-    print("Done!!")
     file_name = input("\nWhat should the downloaded file be called?\n") + '.mp4'
     merging(file_video, file_audio, file_name)
     clearing()
@@ -41,23 +39,22 @@ def download_video(yt, resolution):
 def merging(video, audio, name):
     print("Merging...")
     ffmpeg.concat(video, audio, v=1, a=1).output(name).run()
-    print("Done!!")
 
 
 def clearing():
     print("Removal of residual...")
-    os.remove('temp_v.mp4')
-    os.remove('temp_a.mp4')
+    pathlib.Path('temp_v.mp4').unlink()
+    pathlib.Path('temp_a.mp4').unlink()
     print("Done!!!")
 
 
 def get_url():
     while True:
         link = input("Paste (or enter) the link to the YouTube video\n")
-        if 'youtube' in link:
-            return YouTube(link)
-        else:
+        if 'youtube' not in link:
             print("This is not a link to a YouTube video!\nPlease try again")
+        else:
+            return YouTube(link)
 
 
 def main():
