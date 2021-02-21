dedf1193b78e7d8c6cc1e774117131d27342bc65
JakubKujawa <Kujawa_Jakub@yahoo.com>
2021-02-21 14:54:10 +0100

    Minimize the risk of providing an incorrect link

--- YouTube Video Downloader/YouTube_Video_Downloader.py
+++ YouTube Video Downloader/YouTube_Video_Downloader.py
@@ -2,14 +2,15 @@
 # Date: 17.02.2021
 # IMPORTANT: You need to install ffmpeg for the audio to video merging to take place!
 from pytube import YouTube
+from pytube.exceptions import RegexMatchError
 import convert
 import pathlib
 
 
 def download_audio(yt):
-    file_name = input("\nWhat should the downloaded audio file be called?\n")
+    name_audio = input("\nWhat should the downloaded audio file be called?\n")
     print("Downloading audio...")
-    yt.streams.filter(mime_type="audio/mp4").first().download(filename=file_name)
+    yt.streams.filter(mime_type="audio/mp4").first().download(filename=name_audio)
 
 
 def download_video(yt, res):
@@ -31,10 +32,10 @@ def download_video(yt, res):
     print("Downloading audio...")
     yt.streams.filter(mime_type="audio/mp4").first().download(output_path='Cache',
                                                               filename='temp_a')
-    output()
+    join_files()
 
 
-def output():
+def join_files():
     name_video = input("\nWhat should the downloaded file be called?\n") + '.mp4'
     path_video = pathlib.Path("Cache").joinpath("temp_v.mp4")
     path_audio = pathlib.Path("Cache").joinpath("temp_a.mp4")
@@ -42,7 +43,6 @@ def output():
 
 
 def get_res(yt):
-    global res_list
     res_list = []
     for x in yt.streams.filter(adaptive=True):
         check = x.resolution
@@ -55,11 +55,12 @@ def get_res(yt):
 
 def get_url():
     while True:
-        link = input("Paste (or enter) the link to the YouTube video\n")
-        if 'youtube' not in link:
-            print("This is not a link to a YouTube video!\nPlease try again")
-        else:
+        try:
+            link = input("Paste (or enter) the link to the YouTube video\n")
             return YouTube(link)
+        except RegexMatchError:
+            print("This is not a link to the YouTube video or is incorrect" 
+                  "\nPlease try again")
 
 
 def main():
