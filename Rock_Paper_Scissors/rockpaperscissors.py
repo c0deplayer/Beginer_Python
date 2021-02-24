f8d4c2643056e5108758ec2ad456d39edb3f8c4a
JakubKujawa <Kujawa_Jakub@yahoo.com>
2021-02-24 18:24:06 +0100

    Added new mini-game

---
+++ Rock_Paper_Scissors/rockpaperscissors.py
@@ -0,0 +1,61 @@
+# Author: CodePlayer
+# Date: 23.02.2021
+import random
+import time
+
+rps_table = [["?", 1, 0], [1, "?", 2], [0, 2, "?"]]
+game_rule = {0: 'rock', 1: 'paper', 2: 'scissors'}
+
+
+def player_input():
+    while True:
+        player = input("> ")
+        if player in str(game_rule):
+            return int(player)
+
+        print("Invalid input!")
+
+
+def play():
+    rounds = 0
+    print('''
+    +====================================+
+       Type:
+        > 0 -> Rock
+        > 1 -> Paper 
+        > 2 -> Scissors
+        Result: Player vs. Computer
+    +====================================+ 
+    ''')
+    while rounds != 5:
+        move_player = player_input()
+        move_comp = random.randint(0, 2)
+        print(f"{game_rule[move_player].upper()} vs. {game_rule[move_comp].upper()}")
+
+        winner = rps_table[move_player][move_comp]
+        if winner != "?":
+            print(f"The {'player' if winner == move_player else 'computer'}"
+                  " has won this round!")
+        else:
+            print("Tie!")
+        rounds += 1
+
+
+def menu():
+    while True:
+        print('''
+            ===============================================
+                   Rock, Paper, Scissors (mini-game)       
+            ===============================================
+            ''')
+        time.sleep(1.5)
+        print("\n" * 50)
+        play()
+        leave = input("\nDo you want to quit the mini-game [y/n]?\n> ").lower()
+        if leave in {'y', 'yes', 'quit'}:
+            print("Have a good day/night ^w^")
+            break
+
+
+if __name__ == '__main__':
+    menu()
