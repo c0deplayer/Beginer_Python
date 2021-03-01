01a446e458b707025e610e84db0249b842fae1d4
JakubKujawa <Kujawa_Jakub@yahoo.com>
2021-03-01 22:21:52 +0100

    Added password generator These passwords are unlikely to be remembered, so it's better to use a password manager (unless you don't have one)

---
+++ Password_generator/generator.py
@@ -0,0 +1,39 @@
+# Author: CodePlayer
+# Date: 01.03.2021
+import string
+import random
+
+
+def generator(n, lenght):
+    if n == '1':
+        chars = string.ascii_letters + string.digits
+    else:
+        chars = string.ascii_letters + string.digits + string.punctuation
+
+    return "".join(random.sample(chars, lenght))
+
+
+def main():
+    while True:
+        print('''
+        +===================================+
+            Welcome to Password Generator
+        +===================================+
+          1. Letters + Digits
+          2. Everything w/o whitespaces
+          3. Exit
+        ''')
+        choice = input("Enter one of the available option\n> ")
+        if choice == '3':
+            print('Have a good day/night ^w^')
+            break
+
+        if choice in {'1', '2'}:
+            lenght = int(input("Enter the password length\n> "))
+            print(f"\n>>>> {generator(choice, lenght)}")
+        else:
+            print("Invalid option\n")
+        
+
+if __name__ == "__main__":
+    main()
