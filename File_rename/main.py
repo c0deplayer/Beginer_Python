20bead1db880de0cc5dd49e44a7e2477a449ed7c
JakubKujawa <Kujawa_Jakub@yahoo.com>
2021-03-17 12:13:39 +0100

    Added 'FileExistsError' error catching

--- File_rename/main.py
+++ File_rename/main.py
@@ -10,18 +10,27 @@ def main():
               Files rename tool       
         +---------------------------+
         ''')
-        suc = 0
         path = input("Enter the path of the files (e.g. C:\Screenshots\Test) \n> ") + "\\"
+        success = 0
+        lenght_dir = len(os.listdir(path))
+        print(f"|  There are {lenght_dir} files in this location  |")
         name = input("Enter the new files name \n> ")
-        ext = "." + input("Enter the file extension (e.g. 'txt') \n> ")
+        ext = "." + input("Enter the files extension (e.g. 'txt') \n> ")
         print("Renaming files...")
-
+         
         for count, filename in enumerate(os.listdir(path)):
-            new = path + (name + str(count + 1) + ext)
-            old = path + filename
-            os.rename(old, new)
-            
-        print("Done!")
+            try:
+                new = path + (name + str(count + 1) + ext)
+                old = path + filename
+                os.rename(old, new)
+
+                if os.path.isfile(new):
+                    success += 1
+
+            except FileExistsError as fee:
+                print(fee)
+
+        print(f"\nRenamed files:  Successfully {success}  Failed {lenght_dir - success}\n")
         cont = input("Do you want to continue [y/n]?  ").lower()
         if not cont in {"y", "yes"}:
             print("Have a good day/night")
