0a40af3d561f77184e0aff4a86b5f45e9e9d7bb3
JakubKujawa <Kujawa_Jakub@yahoo.com>
2021-03-20 19:42:05 +0100

    Some improvements > Simple protection against non-existent directory locations > Option to specify the number of files to be renamed > Changed how path and file extension should be specified.

--- File_rename/main.py
+++ File_rename/main.py
@@ -4,7 +4,7 @@ import os
 import re
 
 
-def renaming(path, name, ext):
+def renaming(path, name, ext, num):
     renamed_file = 0
 
     for count, filename in enumerate(os.listdir(path)):
@@ -13,6 +13,9 @@ def renaming(path, name, ext):
             old = path + filename
             os.rename(old, new)
             renamed_file += 1
+
+            if count == num:
+                break
         except FileExistsError as exc:
             print(exc)
     return renamed_file
@@ -28,28 +31,35 @@ def sanitised_input(prompt, regex):
 
 def main():
     while True:
-        print('''
-        +---------------------------+
-              Files rename tool       
-        +---------------------------+
-        ''')
-        path = sanitised_input(
-            "Enter the path of the files (e.g. C:\\Screenshots\\Test\\) \n> ",
-            r"^[a-zA-Z]:[\\\/](?:[\w]+[\\\/])*([\w]+)\\$")
-        lenght_dir = len(os.listdir(path))
-        print(f"|  There are {lenght_dir} files in this location  |")
-        name = sanitised_input(
-            "Enter the new files name \n> ", r"^[\w\-.][\w\-. ]*$")
-        ext = sanitised_input(
-            "Enter the files extension (e.g. '.txt') \n> ", r"^\.[a-zA-Z0-9]+$")
-        print("Renaming files...")
-        success = renaming(path, name, ext)
-        print(
-            f"\nRenamed files:  Successfully {success}  Failed {lenght_dir - success}\n")
-        cont = input("Do you want to continue [y/n]? ").lower()
-        if not cont in {"y", "yes"}:
-            print("Have a good day/night")
-            break
+        try:
+            print('''
+            +---------------------------+
+                  Files rename tool       
+            +---------------------------+
+            ''')
+            path = sanitised_input(
+                "Enter the path of the files (e.g. C:\\Screenshots\\Test) \n> ",
+                r"^[a-zA-Z]:[\\\/](?:[\w]+[\\\/])*([\w]+)$")
+            path += "\\"
+            print(
+                f"|  There are {len(os.listdir(path))} files in this location  |")
+            name = sanitised_input(
+                "Enter the new files name \n> ", r"^[\w\-.][\w\-. ]*$")
+            ext = sanitised_input(
+                "Enter the files extension (e.g. 'txt') \n> ", r"^[a-zA-Z0-9]+$")
+            ext = "." + ext
+            num = int(sanitised_input(
+                "How many files should I rename (alphabetical sorting) \n> ", r"^[1-9]\d*$"))
+            print("Renaming files...")
+            success = renaming(path, name, ext, num - 1)
+            print(
+                f"\nRenamed files:  Successfully {success}  Failed {num - success}\n")
+            cont = input("Do you want to continue [y/n]? ").lower()
+            if not cont in {"y", "yes"}:
+                print("Have a good day/night")
+                break
+        except FileNotFoundError as exc:
+            print(exc)
 
 
 if __name__ == "__main__":
